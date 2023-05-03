3bcb4eb03d10c3d9cf4fd873b5e211558262a81e
MIDESATECH -JDEPAZ- <39348976+midesatech@users.noreply.github.com>
2023-05-02 22:45:27 -0500

    Add code coverage (#1)
    * Add multi module configuration. Add jacoco coverage
    
    * Add module for code coverage
    
    * Remove MAVEN POM vulnerabilities
    
    * Add coverall github action
    
    ---------
    
    Co-authored-by: josedepaz <jose.depaz@live.com>

--- application/src/main/java/com/mdt/LocalApplication.java
+++ application/src/main/java/com/mdt/LocalApplication.java
@@ -1,4 +1,4 @@
-package com.mdt.local;
+package com.mdt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LocalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LocalApplication.class, args);
+    }
 
 }
